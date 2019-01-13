7b6598c923981e44be938e9df86ec6add7131ccb
Michal Bultrowicz <michal.bultrowicz@gmail.com>
2019-01-13 20:26:04 +0000

    test_docker_ip doesn't call fixtures directly.
    Because docker_ip is a session scoped fixture, to reset it we
    need to run it in an isolated test session every time.

--- tests/test_docker_ip.py
+++ tests/test_docker_ip.py
@@ -4,33 +4,44 @@
 import mock
 import pytest
 
+import pytest_docker
 from pytest_docker import docker_ip
 
-
-def test_docker_ip_native():
-    environ = {}
-    with mock.patch('os.environ', environ):
-        assert docker_ip() == '127.0.0.1'
+IP_FIXTURE_NAME = docker_ip.__name__
 
 
-def test_docker_ip_remote():
-    environ = {
-        'DOCKER_HOST': 'tcp://1.2.3.4:2376',
-    }
-    with mock.patch('os.environ', environ):
-        assert docker_ip() == '1.2.3.4'
+def test_docker_ip_native(monkeypatch, testdir):
+    monkeypatch.setenv('DOCKER_HOST', '')
+
+    testdir.makepyfile("""
+    def test_docker_ip(docker_ip):
+        assert docker_ip == '127.0.0.1'
+    """)
+
+    result = testdir.runpytest()
+    result.assert_outcomes(passed=1)
 
 
-@pytest.mark.parametrize('docker_host', [
-    'http://1.2.3.4:2376',
-])
-def test_docker_ip_remote_invalid(docker_host):
-    environ = {
-        'DOCKER_HOST': docker_host,
-    }
-    with mock.patch('os.environ', environ):
+def test_docker_ip_remote(testdir, monkeypatch):
+    monkeypatch.setenv('DOCKER_HOST', 'tcp://1.2.3.4:2376')
+
+    testdir.makepyfile("""
+    def test_docker_ip(docker_ip):
+        assert docker_ip == '1.2.3.4'
+    """)
+
+    result = testdir.runpytest()
+    result.assert_outcomes(passed=1)
+
+
+def test_docker_ip_remote_invalid(monkeypatch, testdir):
+    monkeypatch.setenv('DOCKER_HOST', 'http://1.2.3.4:2376')
+
+    testdir.makepyfile("""
+    def test_docker_ip_invalid(request):
         with pytest.raises(ValueError) as exc:
-            print(docker_ip())
+            request.getfixturevalue(IP_FIXTURE_NAME)
         assert str(exc.value) == (
             'Invalid value for DOCKER_HOST: "%s".' % (docker_host,)
         )
+    """)
