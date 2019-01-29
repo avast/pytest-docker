a0de60ff2c0117b0ec036303f85477fd945e9b51
Rémi Alvergnat <remi.alvergnat@gfi.fr>
2019-01-29 11:54:39 +0100

    Fix issue on subprocess output parsing on Windows and Python<3.6

--- src/pytest_docker/__init__.py
+++ src/pytest_docker/__init__.py
@@ -21,7 +21,11 @@ def execute(command, success_codes=(0,)):
         output = error.output or b''
         status = error.returncode
         command = error.cmd
-    output = output.decode('utf-8')
+    try:
+        output = output.decode('utf-8')
+    except UnicodeDecodeError:
+        # Decoding utf-8 from console output may fail (Windows & Python<3.6)
+        pass
     if status not in success_codes:
         raise Exception(
             'Command %r returned %d: """%s""".' % (command, status, output)
