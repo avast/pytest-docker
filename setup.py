f2aad48eb91dcf75a108cfc597038a7047f4c83a
Luminaar <kovykmax@gmail.com>
2020-01-15 18:33:08 +0100

    Relax requirement for setuptools

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
-from sys import exit
-from setuptools import setup, version
-
-
-if version.__version__ < "40.1.0":
-    exit(
-        "At least version 40.1.0 of setuptools is required ({} is installed)".format(
-            version.__version__
-        )
-    )
+from setuptools import setup
 
 setup(
     setup_requires=["wheel >= 0.32", "pytest-runner >= 5.0, <6.0"],
