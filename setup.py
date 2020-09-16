38ac45e99fcbe000894f7e1f1729f55d4bea92f4
Luminaar <kovykmax@gmail.com>
2020-09-16 21:31:15 +0200

    Update package definitions
    Bump dependencies
    Add requirement for setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
-from setuptools import setup
+from sys import exit
+
+from setuptools import setup, version
+
+if version.__version__ < "40.1.0":
+    exit(
+        "At least version 40.1.0 of setuptools is required ({} is installed)".format(
+            version.__version__
+        )
+    )
+
 
 setup(
     setup_requires=["wheel >= 0.32"],
