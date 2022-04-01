753c7795386b6c5973b5ee317a1353feb106f534
Max <Luminaar@users.noreply.github.com>
2022-04-01 11:43:38 +0200

    Update imports

--- src/pytest_docker/__init__.py
+++ src/pytest_docker/__init__.py
@@ -3,5 +3,15 @@ from .plugin import (
     docker_compose_file,
     docker_compose_project_name,
     docker_ip,
+    docker_setup,
     docker_services,
 )
+
+__all__ = [
+    "docker_cleanup",
+    "docker_compose_file",
+    "docker_compose_project_name",
+    "docker_ip",
+    "docker_setup",
+    "docker_services",
+]
